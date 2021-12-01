39893869edc21372763260aa77143b1df33d8986
dmitry yudakov <dmitryyudakov@yandex-team.ru>
2021-12-01 08:17:42 +0300

    Initial commit

---
+++ Day1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1 {
+    class Program {
+        static void Main(string[] args) {
+            int prev = int.MaxValue;
+            var ans = File.ReadLines("input")
+                .Select(int.Parse)
+                .Count(l => {
+                    var r = l > prev;
+                    prev = l;
+                    return r;
+                });
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ Day1_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1_2 {
+    class Program {
+        static void Main(string[] args) {
+            var depths = File.ReadLines("input").Select(int.Parse).ToArray();
+            int sum = 0;
+            for (int i = 0; i < 3; i++) {
+                sum += depths[i];
+            }
+            int ans = 0;
+            for (int i = 3; i < depths.Length; i++) {
+                var prevSum = sum;
+                sum -= depths[i - 3];
+                sum += depths[i];
+                if (sum > prevSum) {
+                    ans++;
+                }
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
