69e5648f17ee0b987dad0bef96ea02497e0e3c2e
dmitry yudakov <dmitryyudakov@yandex-team.ru>
2021-12-06 22:15:28 +0300

    Day 6

---
+++ Day6_1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day6_1 {
+    class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadLines("input")
+                .Select(l => l.Split(",", StringSplitOptions.RemoveEmptyEntries))
+                .ToArray();
+            var fishes = lines[0].Select(l => int.Parse(l)).ToList();
+            long[] ff = new long[9];
+            foreach (int f in fishes) {
+                ff[f]++;
+            }
+            for (int d = 0; d < 256; d++) {
+                long[] newFF = new long[9];
+                for (int i = 0; i < 9; i++) {
+                    newFF[i] = ff[(i + 1) % 9];
+                }
+                newFF[6] += ff[0];
+                ff = newFF;
+            }
+            Console.WriteLine(ff.Sum());
+        }
+    }
+}
