a353c14cb8b5764e06ce506cadbca68f838da32d
dmitry yudakov <dmitryyudakov@yandex-team.ru>
2021-12-07 08:24:00 +0300

    Day 7

---
+++ Day7_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day7_1 {
+    class Program {
+        static void Main(string[] args) {
+            var nums = File.OpenText("input").ReadLine().Split(",").Select(int.Parse).ToArray();
+            Array.Sort(nums);
+            int i = nums[nums.Length / 2];
+            long ans = 0;
+            foreach (int num in nums) {
+                ans += Math.Abs(num - i);
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ Day7_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day7_2 {
+    class Program {
+        static void Main(string[] args) {
+            var nums = File.OpenText("input").ReadLine().Split(",").Select(int.Parse).ToArray();
+            Array.Sort(nums);
+            int min = nums[0];
+            int max = nums[^1];
+            long ans = long.MaxValue;
+            for (int i = min; i <= max; i++) {
+                long s = 0;
+                foreach (int num in nums) {
+                    var d = Math.Abs(num - i);
+                    long dd = d * (d + 1) / 2;
+                    s += dd;
+                }
+                ans = Math.Min(ans, s);
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
